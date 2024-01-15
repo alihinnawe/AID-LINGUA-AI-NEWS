951cd23cf3d7a44df4ba97980a437276792f4393
alihinnawe <137767987+alihinnawe@users.noreply.github.com>
2024-01-15 18:52:09 +0100

    fix the dateFrom second attempt

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -12,8 +12,8 @@ import AddComment from "../../components/AddComment/";
 ></link>;
 
 const currentDate = new Date();
-const yesterday = new Date("2023-03-06");
-yesterdayFormatted.setDate(yesterday.getDate() - 1);
+const yesterdayFormatted = new Date("2023-03-06").toISOString().substring(0, 10);
+yesterdayFormatted.setDate(yesterdayFormatted.getDate() - 1);
 const yesterday = yesterdayFormatted.toISOString().substring(0, 10);
 const currentDate1 = new Date();
 currentDate.setDate(currentDate1.getDate());
